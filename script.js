4a8dbae504cfc69ef98bb8b85e85b9781d8f417b
SchoolKitsune <kitsune2105skole@gmail.com>
2022-10-07 13:51:14 +0200

    Functionality

--- script.js
+++ script.js
@@ -3,35 +3,50 @@ soda_EL = document.querySelector("#soda");
 soda_num_EL = document.querySelector("#soda_num");
 cursor_EL = document.querySelector("#cursor_img");
 cursor_num_EL = document.querySelector("#cursor_num");
+cursor_price_EL = document.querySelector("#cursor_price");
 
 console.log(soda_num_EL);
 
 soda = 0;
 cursor = 0;
-rain = 0;
+bucket = 0;
 river = 0;
 lake = 0;
 ocean = 0;
 
 cursor_price = 15;
-
+bucket_price = 100;
+cursor_num_EL.innerHTML = "Number of cursors: " + cursor;
+cursor_price_EL.innerHTML = "Price: " + Math.round(cursor_price)
+bucket_num_EL.innerHTML = "Number of buckets: " + cursor;
+bucket_price_EL.innerHTML = "Price: " + Math.round(bucket_price)
 
 function soda_click(){ //each time you click you gain soda
     soda++;
 }
 
+
+
 function buy_cursor(){
     if (soda >= cursor_price){
             cursor++;
-            soda = soda * 1.15
         soda -= cursor_price;
-        
-
-
-
-
+        cursor_num_EL.innerHTML = "Number of cursors: " + cursor;
+        cursor_price *= 1.15
+        cursor_price_EL.innerHTML = "Price: " + Math.round(cursor_price)
     }
     
+}
+
+function buy_bucket(){
+    if (soda >= bucket_price){
+            bucket++;
+       soda -= bucket_price;
+       bucket_num_EL.innerHTML = "Number of buckets: " + cursor;
+       bucket_price *= 1.15
+       bucket_price_EL.innerHTML = "Price: " + Math.round(bucket_price)
+    }
+
 }
 
 soda_EL.addEventListener("click", soda_click); // when we click the soda
@@ -45,6 +60,6 @@ function myBuildingTimer(){
 
 var timer = setInterval(myTimer, 10); // runs the myTimer function 1 time a second
 function myTimer(){ 
-    soda_num_EL.innerHTML = soda;
+    soda_num_EL.innerHTML = Math.round(soda)
     
 }
