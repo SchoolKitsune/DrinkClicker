d228244ab83f817f98148c2bef463ddb24cb27fb
SchoolKitsune <kitsune2105skole@gmail.com>
2022-10-21 10:16:49 +0200

    Watermill
    Watermill

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ soda_num_EL = document.querySelector("#soda_num");
 cursor_EL = document.querySelector("#cursor_img");
 cursor_num_EL = document.querySelector("#cursor_num");
 cursor_price_EL = document.querySelector("#cursor_price");
+bucket_EL = document.querySelector("#bucket_img");
+bucket_num_EL = document.querySelector("#bucket_num");
+bucket_price_EL = document.querySelector("#bucket_price");
 
 console.log(soda_num_EL);
 
@@ -13,6 +16,9 @@ bucket = 0;
 river = 0;
 lake = 0;
 ocean = 0;
+
+cursor_potency = 1;
+bucket_potency = 10;
 
 cursor_price = 15;
 bucket_price = 100;
@@ -42,7 +48,7 @@ function buy_bucket(){
     if (soda >= bucket_price){
             bucket++;
        soda -= bucket_price;
-       bucket_num_EL.innerHTML = "Number of buckets: " + cursor;
+       bucket_num_EL.innerHTML = "Number of buckets: " + bucket;
        bucket_price *= 1.15
        bucket_price_EL.innerHTML = "Price: " + Math.round(bucket_price)
     }
@@ -51,10 +57,23 @@ function buy_bucket(){
 
 soda_EL.addEventListener("click", soda_click); // when we click the soda
 cursor_EL.addEventListener("click", buy_cursor)
+bucket_EL.addEventListener("click", buy_bucket)
 
-var building_timer = setInterval(myBuildingTimer, 1000); // runs the myTimer function 1 time a second
-function myBuildingTimer(){ 
-    soda += cursor;
+var cursor_timer = setInterval(cursortimer, 1000); // runs the myTimer function 1 time a second
+function cursortimer(){ 
+    soda += cursor*cursor_potency;
+
+}
+
+var bucket_timer = setInterval(buckettimer, 1000); // runs the myTimer function 1 time a second
+function buckettimer(){ 
+    soda += bucket*bucket_potency;
+
+}
+
+var bucket_timer = setInterval(buckettimer, 1000); // runs the myTimer function 1 time a second
+function buckettimer(){ 
+    soda += watermill*watermill_potency;
 
 }
 
