c936cd50b4ea9d3cd2742f6a0235a484a3d5f0b5
SchoolKitsune <kitsune2105skole@gmail.com>
2022-11-07 09:42:51 +0100

    Nearly Finished
    Added some descriptions for my code, might add some more finishing touches

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-soda_EL = document.querySelector("#soda");
-soda_num_EL = document.querySelector("#soda_num");
+soda_EL = document.querySelector("#soda"); //soda is how many sodas you have
+soda_num_EL = document.querySelector("#soda_num"); //soda_num is how many sodas being displayed
 cursor_EL = document.querySelector("#cursor_img");
 cursor_num_EL = document.querySelector("#cursor_num");
 cursor_price_EL = document.querySelector("#cursor_price");
@@ -29,7 +29,12 @@ cursor_upgrade_price_1_EL = document.querySelector("#cursor_upgrade_price_1");
 soda_stat_num_EL = document.querySelector("#soda_stat_num");
 times_clicked_num_EL = document.querySelector("#times_clicked_num");
 
+
+//Useful for testing purposes
+
 console.log(soda_num_EL);
+
+//Variables
 
 soda = 0;
 cursor = 0;
@@ -97,7 +102,8 @@ times_clicked_num = 0
 
 cursor_upgrade_price_1_EL.innerHTML = "Price: " + Math.round(cursor_upgrade_price_1)
 
-//Price math and info about buildings 
+//Price math and info about buildings
+//This place also sets info about buildings
 
 cursor_num_EL.innerHTML = "Number of cursors: " + cursor;
 cursor_price_EL.innerHTML = "Price: " + Math.round(cursor_price)
@@ -114,7 +120,7 @@ alchemy_lab_price_EL.innerHTML = "Price: " + Math.round(alchemy_lab_price)
 soda_ocean_num_EL.innerHTML = "Number of soda oceans: " + cursor;
 soda_ocean_price_EL.innerHTML = "Price: " + Math.round(soda_ocean_price)
 
-function soda_click(){ //each time the conditions for the function are met, add soda
+function soda_click(){ //Each time the conditions for the function are met, add soda
     soda++;
     soda_stat_num++;
     times_clicked_num++;
@@ -135,7 +141,7 @@ function buy_cursor_upgrade_1(){
 //Functions for when you buy buildings
 
 function buy_cursor(){
-    if (soda >= cursor_price){
+    if (soda >= cursor_price){ //>= means if soda is greater or equal to the price do the following
             cursor++;
         soda -= cursor_price;
         cursor_num_EL.innerHTML = "Number of cursors: " + cursor;
@@ -210,7 +216,7 @@ function buy_soda_ocean(){
     }
 
 }
-// when we click the right button run the coresponding function
+// when we click the right button run the corresponding function
 soda_EL.addEventListener("click", soda_click);
 cursor_EL.addEventListener("click", buy_cursor)
 bucket_EL.addEventListener("click", buy_bucket)
@@ -222,9 +228,9 @@ soda_ocean_EL.addEventListener("click", buy_soda_ocean)
 cursor_upgrade_1_EL.addEventListener("click", buy_cursor_upgrade_1)
 
 
-var cursor_timer = setInterval(cursortimer, 1000); // runs the myTimer function 1 time a second
-function cursortimer(){ 
-    soda += cursor*cursor_potency;
+var cursor_timer = setInterval(sodatimer, 1000); // runs the soda function each 1000 milliseconds
+function sodatimer(){ 
+    soda += cursor*cursor_potency; //+= replaces the value of a variable which is 0 for example cursor but not for cursor potency
     soda += bucket*bucket_potency;
     soda += soda_river*soda_river_potency;
     soda += brewery*brewery_potency;
@@ -240,8 +246,8 @@ function cursortimer(){
     soda_stat_num += soda_ocean*soda_ocean_potency;
 
 }
-
-var timer = setInterval(myTimer, 10); // runs the myTimer function 1 time a second
+//updates how many sodas being displayed
+var timer = setInterval(myTimer, 10); // runs the myTimer function each 10 milliseconds
 function myTimer(){ 
     soda_num_EL.innerHTML = Math.round(soda);
     soda_stat_num_EL.innerHTML = Math.round(soda_stat_num);
